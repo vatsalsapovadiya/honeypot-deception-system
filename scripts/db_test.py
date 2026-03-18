8ff7aad8c2c2a75fb45aee7ad252cc6d396097e9
Vatsal Sapovadiya <116191183+vatsalsapovadiya@users.noreply.github.com>
2026-03-19 01:14:58 +0700

    Update db_test.py

--- scripts/db_test.py
+++ scripts/db_test.py
@@ -1,1 +1,23 @@
+import sqlite3
+from datetime import datetime
 
+db = "../database/attacks.db"
+conn = sqlite3.connect(db)
+c = conn.cursor()
+
+c.execute("""
+INSERT INTO ssh_attacks
+(timestamp, src_ip, username, password, command)
+VALUES (?, ?, ?, ?, ?)
+""", (
+    datetime.now().isoformat(),
+    "192.168.1.100",
+    "root",
+    "toor",
+    "uname -a"
+))
+
+conn.commit()
+conn.close()
+
+print("[+] Test insert successful")
